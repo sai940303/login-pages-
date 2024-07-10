f78fa19c2276051990b073ed93e5810e09df89ad
sai940303 <sai940303@gmail.com>
2024-07-10 10:43:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='logo-box'>
+        <img src="https://th.bing.com/th/id/R.b757f2bca8039d4a08882d38f4c1b8f0?rik=ghH7DAGg25Eeew&riu=http%3a%2f%2fpluspng.com%2fimg-png%2ftwitter-png-logo-logo-twitter-in-png-2500.png&ehk=8T3AUm4YM1VPbzANgHJr0FTLLw4och8GO%2bj%2fFN9SSrA%3d&risl=&pid=ImgRaw&r=0" alt="logo" className='logo'/>
+       <h2>sign in to twitter</h2>
+      <button>
+        <img src="https://th.bing.com/th/id/OIP.HgH-NjiOdFOrkmwjsZCCfAHaHl?w=2400&h=2456&rs=1&pid=ImgDetMain" alt="logo" />
+        Sign in with Googel
+      </button>
+
+
+      <button>
+        <img src="https://th.bing.com/th/id/OIP.pW0TOZyl_OZMIuo5HjUsyQHaIf?rs=1&pid=ImgDetMain" alt="logo" />
+        Sign in with Googel
+      </button>
+      <hr />
+      <span>Or</span>
+      <form action="">
+        <input type="text" placeholder='phone email Or Username' />
+        <button>next</button>
+      </form>
+      <button>Forget password</button>
+      <p>Don't Have an account <a href="">sing up</a></p>
+
+
+       
+       
+      
+
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
